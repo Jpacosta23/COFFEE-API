4788ce4418e61dae712a189989c97bb48720fb50
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2022-05-17 22:36:35 -0500

    section 2 finished

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+
+    return this.coffeesService.findOne('' + id);
+  }
+
+  @Post()
+  create(@Body() body: CreateCoffeeDto) {
+    console.log(body instanceof CreateCoffeeDto);
+
+    return this.coffeesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
