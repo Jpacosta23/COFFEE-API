e3ef5dd3fcff392fc7a9aa5896cf4f5958ea1ae1
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2022-05-24 08:08:36 -0500

    mongoose connection with CRUD created and implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,22 @@
-export class Coffee {
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   id: number;
+
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop({ default: 0 })
+  recommendations: number;
+
+  @Prop([String])
   flavors: string[];
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(Coffee);
